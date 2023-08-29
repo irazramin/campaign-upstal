24bea709ae68ae95424b7340f9fa27f8c1398793
irazramin <irazramin@gmail.com>
2023-08-29 23:40:09 +0600

    email added

--- src/app/auth/linkedin/callback/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import React from 'react';
-import {LinkedInCallback} from "react-linkedin-login-oauth2";
-
-const callback = () => {
-    return LinkedInCallback
-};
-
-export default callback;
